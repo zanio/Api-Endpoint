acbd796308bffafaf4044eca138c595c2219d575
Zanio <akp.ani@yahoo.com>
2019-06-09 15:31:00 +0100

    removed config from gitignore file

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports ={
+    numPrefix1:23401,
+    numPrefix2:'403297180513',
+    char:'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890',
+    secretKey:'AHHHvvvCCC223!!@@&&*8sddn5Arjre1122334XSwe',
+    secretKey2:'ssdddererbct4AHHbvsiuewfro38grh3kjaxcX;cker34t241456C223!!@@&&*8sddn5Arjre1122334XSwe'
+    
+}

aef620c72a3c0844a88ab9186603a05f94e4d0b9
Zanio <akp.ani@yahoo.com>
2019-06-09 07:03:01 +0100

    wrote test into function.spec.js to test each function

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,12 +2,7 @@ import chai from 'chai';
 import {expect} from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../app';
-import { checkLetter } from '../utils/string'
-import checkFloat from '../utils/checkfloat'
 
-
-
-//https://dev.to/asciidev/testing-a-nodeexpress-application-with-mocha--chai-4lho
 chai.use(chaiHttp);
 //Our parent block
 
@@ -26,25 +21,5 @@ chai.use(chaiHttp);
  
   });
 
-  describe('checkLetter()',()=>{
-    it('it checks if checkLetter function returns boolean',(done)=>{
-      let arr = [false, true, false,true,true]
-      expect(checkLetter(arr)).to.equal(false)
-      done();
-    })
-  })
-
-  describe('checkFloat()',()=>{
-    it('it checks if checkFloat function returns false',(done)=>{
-      let arr = 'as445'
-      expect(checkFloat(arr)).to.equal(false)
-      done();
-    })
-
-    it('it checks if checkFloat function returns float',(done)=>{
-      let arr = '445.45'
-      expect(checkFloat(arr)).to.equal(445.45)
-      done();
-    })
-  })
+ 
 
